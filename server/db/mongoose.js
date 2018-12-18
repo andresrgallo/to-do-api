63d2d316b758d5a3f19565246719d2deb68fe6bc
andresrgallo <andresgallo@hotmail.com>
2018-12-18 21:40:35 +1100

    Add mongodb addon

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(
-	process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoList',
+	process.env.MONGOLAB_AQUA_URI || 'mongodb://localhost:27017/TodoList',
 	{ useNewUrlParser: true }
 );
 
