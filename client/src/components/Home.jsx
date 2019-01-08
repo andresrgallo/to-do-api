8ab3aace239f431185923a3ac668673bda79f4d8
andresrgallo <andresgallo@hotmail.com>
2019-01-08 17:08:00 +1100

    Move client to another location

--- client/src/components/Home.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Home = () => {
-	return <h1 id="welcome">Welcome to the todo App</h1>;
-};
-
-export default Home;
--- client/src/components/NotFound.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NotFound = () => {
-	return <h1>Page not found, go back!!!</h1>;
-};
-
-export default NotFound;
