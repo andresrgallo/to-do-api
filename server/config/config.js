9cc6e4abc5ac55dafe235169356058eafd9bd00e
andresrgallo <andresgallo@hotmail.com>
2019-02-18 17:51:18 +1100

    Modify config file

--- server/config/config.js
+++ server/config/config.js
@@ -8,11 +8,3 @@ if (env === 'development' || env === 'test') {
 		process.env[key] = envConfig[key];
 	});
 }
-
-// if (env === 'development') {
-// 	process.env.PORT = 5000;
-// 	process.env.MONGOLAB_AQUA_URI = 'mongodb://localhost:27017/TodoList';
-// } else if (env === 'test') {
-// 	process.env.PORT = 5000;
-// 	process.env.MONGOLAB_AQUA_URI = 'mongodb://localhost:27017/TodoListTest';
-// }
