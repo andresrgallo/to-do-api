575cb5169815717fa2fe377cea82e958e0711a9a
andresrgallo <andresgallo@hotmail.com>
2019-02-18 17:48:08 +1100

    Add more env variables

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,18 @@
 let env = process.env.NODE_ENV || 'development';
 console.log('env-----', env);
 
-if (env === 'development') {
-	process.env.PORT = 5000;
-	process.env.MONGOLAB_AQUA_URI = 'mongodb://localhost:27017/TodoList';
-} else if (env === 'test') {
-	process.env.PORT = 5000;
-	process.env.MONGOLAB_AQUA_URI = 'mongodb://localhost:27017/TodoListTest';
+if (env === 'development' || env === 'test') {
+	const config = require('./config.json');
+	const envConfig = config[env];
+	Object.keys(envConfig).map(key => {
+		process.env[key] = envConfig[key];
+	});
 }
+
+// if (env === 'development') {
+// 	process.env.PORT = 5000;
+// 	process.env.MONGOLAB_AQUA_URI = 'mongodb://localhost:27017/TodoList';
+// } else if (env === 'test') {
+// 	process.env.PORT = 5000;
+// 	process.env.MONGOLAB_AQUA_URI = 'mongodb://localhost:27017/TodoListTest';
+// }
